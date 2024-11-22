5f0bcca12bcbf8088ab65eae88821b3cf989464a
Alves <117163985+alvessh@users.noreply.github.com>
2024-11-22 01:20:38 -0400

    fix: hashed password with login

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,25 +1,30 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { supabase } from '../../lib/supabaseClient';
+import crypto from 'crypto';
+
+function hashPassword(password: string): string {
+    return crypto.createHash('sha256').update(password).digest('hex');
+}
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
         const { email, password } = req.body;
+        
+        // Hash a senha fornecida pelo usuário
+        const hashedPassword = hashPassword(password);
 
         const { data: user, error } = await supabase
             .from('users')
             .select('id, email, password')
             .eq('email', email)
-            .eq('password', password)
+            .eq('password', hashedPassword)
             .single();
 
         if (error || !user) {
             return res.status(401).json({ message: 'Credenciais inválidas' });
         }
-        if (user.password === password) {
+
         return res.status(200).json({ message: 'Login bem-sucedido!', user });
-        } else {
-            return res.status(401).json({ message: 'Credenciais inválidas' })
-        }
     }
 
     res.setHeader('Allow', ['POST']);
